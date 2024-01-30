6b1a0b42cbbafd03650aa40156228bd6941057f4
Murphy <96611012+mofeiatwork@users.noreply.github.com>
2024-01-30 10:08:39 +0800

    [BugFix] recursive check base tables in the view when active mv (#40276)
    Signed-off-by: Murphy <mofei@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/alter/Alter.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/Alter.java
@@ -75,7 +75,6 @@ import com.starrocks.scheduler.TaskManager;
 import com.starrocks.server.GlobalStateMgr;
 import com.starrocks.server.LocalMetastore;
 import com.starrocks.sql.analyzer.Analyzer;
-import com.starrocks.sql.analyzer.AnalyzerUtils;
 import com.starrocks.sql.analyzer.MaterializedViewAnalyzer;
 import com.starrocks.sql.analyzer.SemanticException;
 import com.starrocks.sql.analyzer.SetStmtAnalyzer;
@@ -359,8 +358,8 @@ public class Alter {
                         "\n\nCause an error: %s", materializedView.getName(), viewDefineSql, e.getMessage(), e);
             }
 
-            Map<TableName, Table> tableNameTableMap = AnalyzerUtils.collectAllConnectorTableAndView(mvQueryStatement);
-            List<BaseTableInfo> baseTableInfos = MaterializedViewAnalyzer.getBaseTableInfos(tableNameTableMap, !isReplay);
+            List<BaseTableInfo> baseTableInfos =
+                    MaterializedViewAnalyzer.processBaseTables(mvQueryStatement, !isReplay);
             materializedView.setBaseTableInfos(baseTableInfos);
             materializedView.getRefreshScheme().getAsyncRefreshContext().clearVisibleVersionMap();
             materializedView.onReload();
